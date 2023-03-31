78596c74c1e7cec0ff1a17646f84fd4c294808e4
Charizmatik <akbar20sharipov01@gmail.com>
2023-03-31 17:50:22 +0300

    Added HomeScreen interface Added search among notifications

---
+++ app/src/main/java/com/sharipov/mynotificationmanager/ui/details/DetailsScreen.kt
@@ -0,0 +1,10 @@
+package com.sharipov.mynotificationmanager.ui.details
+
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun DetailsScreen(itemId: String) {
+
+}
--- app/src/main/java/com/sharipov/mynotificationmanager/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sharipov/mynotificationmanager/ui/home/HomeScreen.kt
@@ -1,23 +1,177 @@
 @file:OptIn(ExperimentalMaterial3Api::class)
+@file:Suppress("UNUSED_EXPRESSION")
 
 package com.sharipov.mynotificationmanager.ui.home
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.text.SimpleDateFormat
+import java.util.*
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen() {
-    Scaffold {
-        LazyColumn {
-            items(3) {
-                Text("Text")
+    var searchVisible by remember { mutableStateOf(false) }
+    var searchText by remember { mutableStateOf("") }
+    val notifications = mutableListOf<Notification>()
+
+    repeat (20){
+        val currentTimeMillis = System.currentTimeMillis()
+        val dateFormat = SimpleDateFormat("dd.MM.yyyy HH:mm", Locale.getDefault())
+        val formattedTime = dateFormat.format(Date(currentTimeMillis))
+        Log.d("Time", formattedTime)
+        notifications.add(
+            Notification("Application name", "User name", "Message text", formattedTime)
+        )
+    }
+
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        "Notifications",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon  = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { searchVisible = !searchVisible }) {
+                        Icon(
+                            imageVector = Icons.Filled.Search,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(48.dp))
+                AnimatedVisibility(
+                    visible = searchVisible,
+                    enter = slideInVertically(
+                        initialOffsetY = { -it },
+                        animationSpec = tween(500)
+                    ),
+                    exit = slideOutVertically(
+                        targetOffsetY = { -it },
+                        animationSpec = tween(500)
+                    )) {
+                    OutlinedTextField(
+                        value = searchText,
+                        onValueChange = { searchText = it },
+                        label = { Text("Search") },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        keyboardOptions = KeyboardOptions(
+                            imeAction = ImeAction.Done
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onDone = {
+                                searchVisible = false
+                            }
+                        )
+                    )
+                }
+                LazyColumn(
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(notifications) { item ->
+                        if (searchText.lowercase() in item.appName.lowercase() ||
+                            searchText.lowercase() in item.userName.lowercase() ||
+                            searchText.lowercase() in item.message.lowercase() ||
+                            searchText == "") {
+                            NotificationItem(notification = item)
+                        }
+                    }
+                    item {
+                        Spacer(modifier = Modifier.height(16.dp))
+                    }
+                }
             }
+        }
+    )
+}
+
+data class Notification(
+    val appName: String,
+    val userName: String,
+    val message: String,
+    val time: String
+)
+
+@Composable
+fun NotificationItem(notification: Notification) {
+    Card(
+        elevation = CardDefaults.cardElevation(),
+        modifier = Modifier.padding(16.dp, 16.dp, 16.dp)
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Row(modifier = Modifier.padding(8.dp, 16.dp, 16.dp, 16.dp)) {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "icon",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(8.dp, 8.dp, 16.dp, 8.dp)
+                        .size(56.dp)
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(end = 16.dp)
+                ) {
+                    Text(notification.appName, style = MaterialTheme.typography.titleMedium)
+                    Text(notification.userName, style = MaterialTheme.typography.bodyLarge)
+                    Text(
+                        notification.message,
+                        style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 1
+                    )
+                }
+            }
+            Text(
+                text = notification.time,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp)
+            )
         }
     }
 }
