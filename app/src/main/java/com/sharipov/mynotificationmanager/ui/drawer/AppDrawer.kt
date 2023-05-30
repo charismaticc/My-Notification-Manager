e3bd850ec7bd613b7b2fcafa02c382f7dfbfdbcf
Charizmatik <akbar20sharipov01@gmail.com>
2023-05-30 18:56:26 +0300

    1. Output NavigationDrawerItem to a separate file to shorten the code 2. Replaced icons

--- app/src/main/java/com/sharipov/mynotificationmanager/ui/drawer/AppDrawer.kt
+++ app/src/main/java/com/sharipov/mynotificationmanager/ui/drawer/AppDrawer.kt
@@ -2,10 +2,6 @@ package com.sharipov.mynotificationmanager.ui.drawer
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,9 +9,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sharipov.mynotificationmanager.R
+import com.sharipov.mynotificationmanager.ui.drawer.component.MyNavigationDrawerItem
 import com.sharipov.mynotificationmanager.utils.Constants
 
 @Composable
@@ -33,79 +29,46 @@ fun AppDrawer(
         Spacer(modifier = Modifier.padding(16.dp))
         Column(modifier = modifier) {
 
-            NavigationDrawerItem(
-                label = { Text(text = "Applications", style = MaterialTheme.typography.bodyLarge) },
+            MyNavigationDrawerItem(
+                label = "Applications",
                 selected = route == Constants.Screens.APPLICATION_SCREEN,
                 onClick = {
                     navigateToApplications()
                     closeDrawer()
                 },
-                icon = {
-                    Icon(
-                        modifier = Modifier.size(36.dp),
-                        painter = painterResource(id = R.drawable.ic_apps),
-                        contentDescription = null
-                    )
-                },
-                shape = MaterialTheme.shapes.medium
+                icon = painterResource(id = R.drawable.ic_apps),
             )
-
-            NavigationDrawerItem(
-                label = {
-                    Text(
-                        text = "All notification",
-                        style = MaterialTheme.typography.bodyLarge
-                    )
-                },
+            Spacer(modifier = Modifier.padding(2.dp))
+            MyNavigationDrawerItem(
+                label = "All notification",
                 selected = route == Constants.Screens.ALL_NOTIFICATIONS_SCREEN,
                 onClick = {
                     navigateToAllNotifications()
                     closeDrawer()
                 },
-                icon = {
-                    Icon(
-                        modifier = Modifier.size(36.dp),
-                        imageVector = Icons.Default.Notifications,
-                        contentDescription = null
-                    )
-                },
-                shape = MaterialTheme.shapes.large
+                icon = painterResource(id = R.drawable.ic_notifications),
             )
-
-            NavigationDrawerItem(
-                label = { Text(text = "Favorites", style = MaterialTheme.typography.bodyLarge) },
+            Spacer(modifier = Modifier.padding(2.dp))
+            MyNavigationDrawerItem(
+                label = "Favorites",
                 selected = route == Constants.Screens.FAVORITE_SCREEN,
                 onClick = {
                     navigateToFavorite()
                     closeDrawer()
                 },
-                icon = {
-                    Icon(
-                        modifier = Modifier.size(36.dp),
-                        imageVector = Icons.Default.Star,
-                        contentDescription = null
-                    )
-                },
-                shape = MaterialTheme.shapes.medium
+                icon = painterResource(id = R.drawable.ic_star),
             )
 
             Spacer(modifier = Modifier.weight(1f))
-            NavigationDrawerItem(
-                modifier = Modifier.padding(bottom = 16.dp),
-                label = { Text(text = "Settings", style = MaterialTheme.typography.bodyLarge) },
+
+            MyNavigationDrawerItem(
+                label = "Settings",
                 selected = route == Constants.Screens.SETTINGS_SCREEN,
                 onClick = {
                     navigateToSettings()
                     closeDrawer()
                 },
-                icon = {
-                    Icon(
-                        modifier = Modifier.size(36.dp),
-                        imageVector = Icons.Default.Settings,
-                        contentDescription = null
-                    )
-                },
-                shape = MaterialTheme.shapes.medium
+                icon = painterResource(id = R.drawable.ic_settings),
             )
         }
     }
@@ -129,10 +92,4 @@ fun DrawerHeader(modifier: Modifier) {
             color = MaterialTheme.colorScheme.onPrimary,
         )
     }
-}
-
-@Preview
-@Composable
-fun DrawerHeaderPreview() {
-    AppDrawer(modifier = Modifier, route = Constants.Screens.APPLICATION_SCREEN)
 }
