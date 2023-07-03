a2b595ebdee506e3f8841d7d1236e579fbabd1d0
Charizmatik <akbar20sharipov01@gmail.com>
2023-07-04 01:35:07 +0300

    1. Added descriptions to the block notifications and Save notification

--- app/src/main/java/com/sharipov/mynotificationmanager/ui/notificationmanagement/NotificationManagement.kt
+++ app/src/main/java/com/sharipov/mynotificationmanager/ui/notificationmanagement/NotificationManagement.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -29,6 +30,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -117,7 +119,25 @@ fun NotificationManagement(
                 ) {
                     if (!whichListIsDisplayed.value && !searchVisible) {
                         item {
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = stringResource(id = R.string.block_notifications_rules),
+                                textAlign = TextAlign.Center
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Divider(color = Color.Gray, thickness = 1.dp)
+
                             DoNotDisturbMode(isChecked, context)
+                        }
+                    } else if(!searchVisible) {
+                        item{
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Text(
+                                text = stringResource(id = R.string.save_notifications_rules),
+                                textAlign = TextAlign.Center
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+                            Divider(color = Color.Gray, thickness = 1.dp)
                         }
                     }
                     items(apps) { app ->
