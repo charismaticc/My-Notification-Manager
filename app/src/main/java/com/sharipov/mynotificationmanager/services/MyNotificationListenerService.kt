73ca34afd076b61b8155eb28b2ba469d8d0a140c
Charizmatik <akbar20sharipov01@gmail.com>
2023-07-25 13:17:27 +0300

    1. fix notification service (isExcluded don't work)

--- app/src/main/java/com/sharipov/mynotificationmanager/services/MyNotificationListenerService.kt
+++ app/src/main/java/com/sharipov/mynotificationmanager/services/MyNotificationListenerService.kt
@@ -23,6 +23,7 @@ class MyNotificationListenerService : NotificationListenerService() {
     private lateinit var notificationDao: NotificationDao
     private lateinit var excludedAppDao: ExcludedAppDao
     private lateinit var context: Context
+    private val instagramMods = listOf("Instagram", "Instander")
     override fun onCreate() {
         super.onCreate()
         val database = Room.databaseBuilder(
@@ -38,6 +39,8 @@ class MyNotificationListenerService : NotificationListenerService() {
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         try {
+            // get package Name
+            val packageName = sbn.packageName
             val coroutineScope = CoroutineScope(Dispatchers.IO)
             coroutineScope.launch {
                 val excludedApp = excludedAppDao.getExcludedAppByPackageName(packageName)
@@ -47,91 +50,76 @@ class MyNotificationListenerService : NotificationListenerService() {
                 else if (excludedApp.isBlocked) {
                     cancelNotification(sbn.key)
                 }
-                else if (excludedApp.isExcluded) {
-                    saveNotification(sbn, notificationDao, context, coroutineScope)
+                else {
+                    if (excludedApp.isExcluded) {
+                        // get application name
+                        val pm = context.packageManager
+                        val appName =
+                            pm.getApplicationLabel(
+                                pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
+                            ).toString()
+                        // get user name
+                        val extras = sbn.notification.extras
+                        var group = extras.getString(Notification.EXTRA_CONVERSATION_TITLE) ?: "not_group"
+                        var user = extras.getString(Notification.EXTRA_TITLE)?.replace("/", "-") ?: "Unknown"
+
+                        if (user.trim() == "") {
+                            user = "Unknown"
+                        }
+                        // We change '/' to '-' because the tail gives an error indicating the
+                        // wrong path when switching to a user whose name contains this character
+
+                        // Separating the user name from the account name because instagram is shit
+                        if (appName in instagramMods) {
+                            user = if (user.split(":").size == 2) {
+                                user.split(":")[1].trim()
+                            } else {
+                                user.trim()
+                            }
+
+                            if (group != "not_group") {
+                                group = group.replace(group.split(" ")[0], "")
+                                group = group.trim()
+                            }
+                        } else if (group != "not_group") {
+                            user = user.replace("$group:", "").trim()
+                        }
+
+                        if (group == user) {
+                            group = "not_group"
+                        }
+
+                        // get notification text
+                        var text = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString() ?: ""
+                        val bigText = extras.getCharSequence(Notification.EXTRA_BIG_TEXT)?.toString() ?: ""
+                        if (text.trim() == "") {
+                            text = bigText
+                        } else if (bigText.trim() != "" && text.trim() != "") {
+                            text = bigText
+                        }
+
+                        if (text.isNotEmpty() && group.isNotEmpty()) {
+                            val count = notificationDao.checkNotificationExists(user, text, packageName, appName)
+                            if (count == 0) {
+                                val notificationEntity =
+                                    NotificationEntity(
+                                        id = null,
+                                        appName = appName,
+                                        packageName = packageName,
+                                        group = group,
+                                        user = user,
+                                        text = text,
+                                        time = System.currentTimeMillis(),
+                                        favorite = false
+                                    )
+                                notificationDao.insert(notificationEntity)
+                            }
+                        }
+                    }
                 }
             }
         } catch (e: Exception) {
             e.printStackTrace()
-        }
-    }
-}
-
-fun saveNotification(
-    sbn: StatusBarNotification,
-    notificationDao: NotificationDao,
-    context: Context,
-    coroutineScope: CoroutineScope
-) {
-    val instagramMods = listOf("Instagram", "Instander")
-    val packageName = sbn.packageName
-    val pm = context.packageManager
-
-    val appName =
-        pm.getApplicationLabel(
-            pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA)
-        ).toString()
-
-    // get group and user name
-    val extras = sbn.notification.extras
-//    val groupUserName = getGroupAndUserName(extras, appName)
-//    val group = groupUserName.first
-//    val user =groupUserName.second
-    var group = extras.getString(Notification.EXTRA_CONVERSATION_TITLE) ?: "not_group"
-    var user = extras.getString(Notification.EXTRA_TITLE)?.replace("/", "-") ?: "Unknown"
-
-    if (user.trim() == "") {
-        user = "Unknown"
-    }
-    // We change '/' to '-' because the tail gives an error indicating the
-    // wrong path when switching to a user whose name contains this character
-
-    // Separating the user name from the account name because instagram is shit
-    if (appName in instagramMods) {
-        user = if (user.split(":").size == 2) {
-            user.split(":")[1].trim()
-        } else {
-            user.trim()
-        }
-
-        if (group != "not_group") {
-            group = group.replace(group.split(" ")[0], "")
-            group = group.trim()
-        }
-    } else if (group != "not_group") {
-        user = user.replace("$group:", "").trim()
-    }
-
-    if (group == user) {
-        group = "not_group"
-    }
-
-    // get notification text
-    var text = extras.getCharSequence(Notification.EXTRA_TEXT)?.toString() ?: ""
-    val bigText = extras.getCharSequence(Notification.EXTRA_BIG_TEXT)?.toString() ?: ""
-    if (text.trim() == "") {
-        text = bigText
-    } else if (bigText.trim() != "" && text.trim() != "") {
-        text = bigText
-    }
-
-    if (text.isNotEmpty() && group.isNotEmpty()) {
-        coroutineScope.launch {
-            val count = notificationDao.checkNotificationExists(user, text, packageName, appName)
-            if (count == 0) {
-                val notificationEntity =
-                    NotificationEntity(
-                        id = null,
-                        appName = appName,
-                        packageName = packageName,
-                        group = group,
-                        user = user,
-                        text = text,
-                        time = System.currentTimeMillis(),
-                        favorite = false
-                    )
-                notificationDao.insert(notificationEntity)
-            }
         }
     }
 }
