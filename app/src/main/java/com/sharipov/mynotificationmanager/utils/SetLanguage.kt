602b05df1abcd8988cc048f3914c9ceb810bc415
Charizmatik <akbar20sharipov01@gmail.com>
2023-08-02 00:42:45 +0300

    1. Change language set type

--- app/src/main/java/com/sharipov/mynotificationmanager/utils/SetLanguage.kt
+++ app/src/main/java/com/sharipov/mynotificationmanager/utils/SetLanguage.kt
@@ -6,12 +6,20 @@ import com.sharipov.mynotificationmanager.data.PreferencesManager
 import java.util.Locale
 
 fun setChanges(context: Context ) {
-    val language = when (PreferencesManager.getSelectedLanguage(context)) {
-        "English" -> "en"
-        "Русский" -> "ru"
-        "Тоҷикӣ" -> "tg"
-        "Українська" -> "uk"
-        else -> "en"
+    val languageList = listOf("en", "ru", "tg", "uk")
+    val language: String
+    if(PreferencesManager.getSelectedLanguage(context) == null && getSystemLanguage(context) in languageList)
+    {
+        language = getSystemLanguage(context)
+        PreferencesManager.saveSelectedLanguage(context, language)
+    }else {
+        language = when (PreferencesManager.getSelectedLanguage(context)) {
+            "English" -> "en"
+            "Русский" -> "ru"
+            "Тоҷикӣ" -> "tg"
+            "Українська" -> "uk"
+            else -> "en"
+        }
     }
 
     val locale = Locale(language)
@@ -19,5 +27,10 @@ fun setChanges(context: Context ) {
     val resources = context.resources
     val configuration = Configuration(resources.configuration)
     configuration.setLocale(locale)
+    context.createConfigurationContext(configuration)
     resources.updateConfiguration(configuration, resources.displayMetrics)
+}
+
+fun getSystemLanguage(context: Context): String {
+    return context.resources.configuration.locales.get(0).language
 }
