6eb1fc335143e6c54be83b688ad5237570dcdba1
Charizmatik <akbar20sharipov01@gmail.com>
2023-08-21 11:48:59 +0300

    1. Added translation

--- app/src/main/java/com/sharipov/mynotificationmanager/ui/notificationmanagement/NotificationManagement.kt
+++ app/src/main/java/com/sharipov/mynotificationmanager/ui/notificationmanagement/NotificationManagement.kt
@@ -11,7 +11,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -110,7 +110,7 @@ fun NotificationManagement(
                             textAlign = TextAlign.Center
                         )
                         Spacer(modifier = Modifier.height(8.dp))
-                        Divider(color = Color.Gray, thickness = 1.dp)
+                        HorizontalDivider(thickness = 1.dp, color = Color.Gray)
                     }
                 }
                 item {
@@ -124,7 +124,7 @@ fun NotificationManagement(
                         whichListIsDisplayed = whichListIsDisplayed.value,
                         appEntity = app
                     )
-                    Divider(color = Color.Gray, thickness = 1.dp)
+                    HorizontalDivider(thickness = 1.dp, color = Color.Gray)
                 }
                 item { Spacer(modifier = Modifier.height(60.dp)) }
             }
